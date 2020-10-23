90249d3fbb4f06016b63ee37ea54de25781a0ebc
Aki Van Ness <aki@lethalbit.net>
2020-10-23 00:38:35 -0400

    Added the needed changes in the meson build for the pcap and pcapng implementation files

---
+++ src/libnokogiri/pcap.cc
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+/* pcap.cc - pcap file format interface for libnokogiri */
+
+#include <libnokogiri/pcap.hh>
+
+namespace libnokogiri::pcap {
+
+}
---
+++ src/libnokogiri/pcapng.cc
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+/* pcapng.cc - pcapng file format interface for libnokogiri */
+
+#include <libnokogiri/pcapng.hh>
+
+namespace libnokogiri::pcapng {
+
+}
