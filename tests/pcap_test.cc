777a1d17fbb585b8be7248bf8bb8e5a6e95784b5
Aki Van Ness <aki@lethalbit.net>
2020-10-29 02:38:12 -0400

    Added two test runners, one for pcap files and one for pcapng files

---
+++ tests/pcap_test.cc
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+/* pcap_test.cc - libnokogiri test harness for pcap files */
+
+#include <libnokogiri/pcap.hh>
+
+int main(int argc, char** argv) {
+
+
+
+	return {};
+}
---
+++ tests/pcapng_test.cc
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+/* pcapng_test.cc - libnokogiri test harness for pcapng files */
+
+#include <libnokogiri/pcapng.hh>
+
+int main(int argc, char** argv) {
+
+	return {};
+}
